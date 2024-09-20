3e7073f311ec24a8bccbc63d8945247d648ec73c
Artem Nosov <103992767+avnosov3@users.noreply.github.com>
2024-09-20 21:46:35 +0200

    [data-service] feat(services:accommodation): created AccommodationService

---
+++ data-service/src/services/accommodation.py
@@ -0,0 +1,39 @@
+from functools import lru_cache
+from uuid import UUID
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.models.accommodation import Accommodation
+from src.repositories.accommodation import AccommodationRepository, get_accommodation_repository
+
+
+class AccommodationService:
+    def __init__(
+        self,
+        accommodation_repository: AccommodationRepository,
+    ) -> None:
+        self.accommodation_repository = accommodation_repository
+
+    async def get_accommodation(
+        self,
+        accommodation_id: UUID,
+        session: AsyncSession,
+    ) -> Accommodation:
+        return await self.accommodation_repository.get_accommodation(accommodation_id, session)
+
+    async def get_accommodations(
+        self,
+        session: AsyncSession,
+    ) -> list[Accommodation]:
+        return await self.accommodation_repository.get_all(session)
+
+    async def get_expanded_accommodation(
+        self,
+        accommodation_id: UUID,
+        session: AsyncSession,
+    ) -> Accommodation:
+        return await self.accommodation_repository.get_expanded_accommodation(accommodation_id, session)
+
+
+@lru_cache
+def get_accommodation_service() -> AccommodationService:
+    return AccommodationService(get_accommodation_repository())
