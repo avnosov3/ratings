06704b4905b44586ea54364b4cd2efbb64699a45
Artem Nosov <103992767+avnosov3@users.noreply.github.com>
2024-09-20 19:22:18 +0200

    [data-service] feat(core:db): created async connection

---
+++ data-service/src/core/db.py
@@ -0,0 +1,20 @@
+from typing import Generator
+
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, async_sessionmaker, create_async_engine
+from src.core.config import settings
+
+
+def get_async_engine(db_url: str) -> AsyncEngine:
+    return create_async_engine(url=db_url)
+
+
+def get_async_session_maker(engine: AsyncEngine) -> async_sessionmaker[AsyncSession]:
+    return async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_async_session() -> Generator[AsyncSession, None, None]:
+    engine = get_async_engine(settings.DATABASE_URL)
+    async_session = get_async_session_maker(engine)
+
+    async with async_session() as session:
+        yield session
