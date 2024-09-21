2cb25d29cb6be6c5a6d5bc42c0a074bdb7d4de74
Artem Nosov <103992767+avnosov3@users.noreply.github.com>
2024-09-21 12:28:23 +0200

    [data-service] feat(repositories:review): added get_review_by_accommodation get_reviews_by_accommodation in ReviewRepository

--- data-service/src/repositories/review.py
+++ data-service/src/repositories/review.py
@@ -1,6 +1,7 @@
 from functools import lru_cache
 from uuid import UUID
 
+from sqlalchemy import and_, select
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.models.review import Locale, Review, Source
 
@@ -31,6 +32,32 @@ class ReviewRepository(BaseRepository):
         commit: bool = True,
     ) -> Review:
         return await self.create(in_review, session, commit)
+
+    async def get_reviews_by_accommodation(
+        self,
+        accommodation_id: UUID,
+        session: AsyncSession,
+    ) -> list[Review]:
+        result = await session.execute(select(Review).where(Review.accommodation_id == accommodation_id))
+        reviews = result.scalars().all()
+        return reviews
+
+    async def get_review_by_accommodation(
+        self,
+        accommodation_id: UUID,
+        review_id: UUID,
+        session: AsyncSession,
+    ) -> Review:
+        result = await session.execute(
+            select(Review).where(
+                and_(
+                    Review.accommodation_id == accommodation_id,
+                    Review.id == review_id,
+                ),
+            ),
+        )
+        review = result.scalars().first()
+        return review
 
 
 @lru_cache()
